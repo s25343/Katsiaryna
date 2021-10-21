e4b4dbed7e3cb4135ebc4127857c0991e4b22cf3
s25343 <91895708+s25343@users.noreply.github.com>
2021-10-21 20:39:24 +0200

    Update 2004.cpp

--- src/2004.cpp
+++ src/2004.cpp
@@ -7,15 +7,21 @@ int main()
     int a;
     cout << "Podaj liczbe " << endl;
     cin>>a;
+    if ((a==1) || (a==2) || (a==3) || (a==5) || (a==7))
     {
-    if (a/1; a/a)
-    {
-        cout<<"To pierwsza liczba"<<endl;
+        cout<<"podchodzi"<<endl;
     }
     else
     {
-        cout<<"nie pierwsza"<<endl;
+        cout<<"nie podchodzi"<<endl;
     }
+    if ((a%1==0) || (a%2==0) || (a%3==0) || (a%5==0) || (a%7==0))
+    {
+        cout <<" "<<endl;
+    }
+    else
+    {
+        cout<<" "<<endl;
     }
     return 0;
 }
