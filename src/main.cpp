d0afebb79455e032c69a2f8d572d0e1c86264139
s25343 <91895708+s25343@users.noreply.github.com>
2021-10-21 18:52:40 +0200

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Podaj liczbe " << endl;
+    cin>>a;
+    {
+    if (a/1; a/a)
+    {
+        cout<<"To pierwsza liczba"<<endl;
+    }
+    else
+    {
+        cout<<"nie pierwsza"<<endl;
+    }
+    }
+    return 0;
+}
