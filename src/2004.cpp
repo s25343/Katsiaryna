71e3b696f79382d155e05711e1d5d27ec9060b09
s25343 <91895708+s25343@users.noreply.github.com>
2021-10-22 22:52:45 +0200

    Update 2004.cpp

--- src/2004.cpp
+++ src/2004.cpp
@@ -5,23 +5,19 @@ using namespace std;
 int main()
 {
     int a;
-    cout << "Podaj liczbe " << endl;
+    cout<<"Podaj liczbe"<<endl;
     cin>>a;
-    if ((a==1) || (a==2) || (a==3) || (a==5) || (a==7))
+    if (a>1)
     {
-        cout<<"podchodzi"<<endl;
-    }
-    else
-    {
-        cout<<"nie podchodzi"<<endl;
-    }
-    if ((a%1==0) || (a%2==0) || (a%3==0) || (a%5==0) || (a%7==0))
-    {
-        cout <<" "<<endl;
-    }
-    else
-    {
-        cout<<" "<<endl;
+        for(int i=2; i<a; i++)
+            if (a%i==0)
+        {
+              cout<<"Nie Prawda"<<endl;
+        }
+        else
+        {
+              cout<<"Prawda"<<endl;
+        }
     }
     return 0;
 }
