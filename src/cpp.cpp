7fe67e66bbbd3cfc7e664b496633ed454e59266f
s25343 <91895708+s25343@users.noreply.github.com>
2021-11-04 15:12:47 +0100

    Update cpp.cpp

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -2,12 +2,12 @@
 int main()
 {
 int a,b,c;
-std::cout<<"podaj a"<<std::endl;
+std::cout<<"Podaj a"<<std::endl;
 std::cin>>a;
-std::cout<<"podaj b"<<std::endl;
+std::cout<<"Podaj b"<<std::endl;
 std::cin>>b;
 c=a+b;
-std::cout<<"wynik"<<c<<std::endl;
+std::cout<<"Wynik = "<<c<<std::endl;
 return 0;
 }
 
