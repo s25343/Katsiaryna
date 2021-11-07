33c31e9544476ff0af517c700f188b8ebd0d7810
s25343 <91895708+s25343@users.noreply.github.com>
2021-11-07 16:22:20 +0100

    Update 20011.cpp

--- src/20011.cpp
+++ src/20011.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
+
 using namespace std;
 int main()
 {
-int a;
-cout<< "Koordynaty prostokujata "<<endl;
-cin>>a;
-for (int z=1; z<=2; z++)
-    for (int t=1; t<=a; t++)
-    {
-        cout<<"*";
-    }
-return 0;
+	int a,b;
+	cout<<"Podaj koordynaty wysokosci "<<endl;
+	cin>>a;
+	cout<<"podaj koordynaty dlugosci"<<endl;
+	cin>>b;
+	for(int i=0; i<a; i++)
+	{
+		for(int n=0; n<b; n++)
+		{
+		    cout<<"*";
+		}
+	cout<<endl;
+	}
+	return 0;
 }
