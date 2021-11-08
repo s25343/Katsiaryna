e8065953b071cf9a6b3deef5056e8ab4ac01003e
s25343 <91895708+s25343@users.noreply.github.com>
2021-11-08 18:07:55 +0100

    Create d1.cpp

---
+++ src/d1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+int main()
+{
+int a;
+std::cout<<"Podaj numer "<<std::endl;
+std::cin>>a;
+if (a==1){
+std::cout<<" Poniadzialek"<<std::endl;}
+if (a==2){
+std::cout<<" Wtorek"<<std::endl;}
+if (a==3){
+std::cout<<" Sroda"<<std::endl;}
+if (a==4){
+std::cout<<" Czwartek"<<std::endl;}
+if (a==5){
+std::cout<<" Piatek"<<std::endl;}
+if (a==6){
+std::cout<<" Sobbota"<<std::endl;}
+if (a==7){
+std::cout<<" Niedzziela"<<std::endl;}
+return 0;
+}
+
+zmienic na swich
