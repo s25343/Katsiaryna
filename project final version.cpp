05e23f577227cbdf5e3c0320c3ffacf69e22f964
s25343 <91895708+s25343@users.noreply.github.com>
2022-01-24 16:01:43 +0100

    Update and rename project v3.cpp to project final version.cpp

--- project final version.cpp
+++ project final version.cpp
@@ -2,12 +2,11 @@
 #include <ctime>
 #include <conio.h>
 
-//не забыть вынести все функции в header.h( nie zapomniać renderowac wszystko)
 using namespace std;
 
 char userInput;
 
-short life = 0, steps = 0;
+short steps = 0;
 short playerY = 0, playerX = 0;
 
 short keyPressCount = 0;
@@ -36,7 +35,7 @@ char** genMap(int size, int*& arrSize, char space = '#') {
     arrSize = new int[size];
 
     for (int i = 0; i < size; i++) {
-        arrSize[i] = 5/*5 + rand() % 13*/;                  //заменить в будущем на rand()%(to trzeba będzie zmienic na random)
+        arrSize[i] = 10;
         map[i] = new char[arrSize[i]];
     }
 
@@ -53,13 +52,13 @@ char** genMap(int size, int*& arrSize, char space = '#') {
 
         switch (rand() % 3) {
         case 0:
-            map[indexI][indexJ] = randN('0', '9');  //прибавлять шаги(dodawanie skoków)
+            map[indexI][indexJ] = randN('0', '9');
             break;
         case 1:
-            map[indexI][indexJ] = '*';              //телепорт(teleportacja)
+            map[indexI][indexJ] = '*';
             break;
         case 2:
-            map[indexI][indexJ] = '#';              //ловушка с отнятием жизни(zeby odyjmowac proby)
+            map[indexI][indexJ] = '#';
             break;
         }
     }
@@ -68,13 +67,13 @@ char** genMap(int size, int*& arrSize, char space = '#') {
 }
 
 void showMap(char** map, int size, int* arrSize) {
-    cout << "========= S: " << steps << " ======== <3: " << life << " === " << digitCount << " ==\n\n";
-    for (int i = 0; i < size; i++) {                //ось Y
-        for (int j = 0; j < arrSize[i]; j++)        //X
+    cout << "==== Steps:" << steps << " ==== Score:" << digitCount << " =====\n\n";
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < arrSize[i]; j++)
             cout << map[i][j] << ' ';
         cout << endl;
     }
-    cout << "\n===================================\n\n\n";
+    cout << "\n=================================\n\n\n";
 }
 
 int main()
@@ -86,12 +85,14 @@ int main()
 
     char** map = genMap(size, arrSize, ' ');
     playerY = randN(0, size - 1), playerX = randN(0, arrSize[playerY] - 1);
+
     map[playerY][playerX] = 'Z';
 
     digitCount = countDigit(map, size, arrSize);
-    cout << digitCount << endl;
 
+    short stepsStart = 0;
     steps = size + rand() % (size * size);
+    stepsStart = steps;
     showMap(map, size, arrSize);
 
     char futStep;
@@ -99,18 +100,15 @@ int main()
         if (digitCount <= 0) {
             break;
         }
-
         userInput = _getch();
         keyPressCount++;
-
-        cout << "userInput 13: " << userInput << endl;
-        cout << "playerY: " << playerY << " playerX: " << playerX << endl;
+        //cout << "userInput: " << userInput << endl;
+       // cout << "playerY: " << playerY << " playerX: " << playerX << endl;
 
         switch (userInput) {
         case 'w':
         case 'W': {
             if (playerY > 0) {
-                cout << "up\n";
 
                 futStep = map[playerY - 1][playerX];
                 if (futStep >= '0' && futStep <= '9') {
@@ -123,11 +121,11 @@ int main()
 
                 if (futStep >= '0' && futStep <= '9' || futStep == '#' || futStep == ' ') {
                     map[playerY - 1][playerX] = 'Z';
-                    map[playerY][playerX] = '#';
+                    map[playerY][playerX] = ' ';
                     playerY--;
                 }
                 else if (futStep == '*') {
-                    map[playerY][playerX] = '#';
+                    map[playerY][playerX] = ' ';
                     playerY = randN(0, size - 1), playerX = randN(0, arrSize[playerY] - 1);
 
                     futStep = map[playerY][playerX];
@@ -136,6 +134,7 @@ int main()
                         digitCount--;
                     }
 
+
                     map[playerY][playerX] = 'Z';
                 }
 }
@@ -143,7 +142,6 @@ int main()
         case 'a':
         case 'A': {
             if (playerX > 0) {
-                cout << "left\n";
 
                 futStep = map[playerY][playerX - 1];
                 if (futStep >= '0' && futStep <= '9') {
@@ -156,11 +154,11 @@ int main()
 
                 if (futStep >= '0' && futStep <= '9' || futStep == '#' || futStep == ' ') {
                     map[playerY][playerX - 1] = 'Z';
-                    map[playerY][playerX] = '#';
+                    map[playerY][playerX] = ' ';
                     playerX--;
                 }
                 else if (futStep == '*') {
-                    map[playerY][playerX] = '#';
+                    map[playerY][playerX] = ' ';
                     playerY = randN(0, size - 1), playerX = randN(0, arrSize[playerY] - 1);
 
                     futStep = map[playerY][playerX];
@@ -177,7 +175,6 @@ int main()
         case 's':
         case 'S': {
             if (playerY < size - 1) {
-                cout << "down\n";
 
                 futStep = map[playerY + 1][playerX];
                 if (futStep >= '0' && futStep <= '9'){
@@ -190,11 +187,11 @@ int main()
 
                 if (futStep >= '0' && futStep <= '9' || futStep == '#' || futStep == ' ') {
                     map[playerY + 1][playerX] = 'Z';
-                    map[playerY][playerX] = '#';
+                    map[playerY][playerX] = ' ';
                     playerY++;
                 }
                 else if (futStep == '*') {
-                    map[playerY][playerX] = '#';
+                    map[playerY][playerX] = ' ';
                     playerY = randN(0, size - 1), playerX = randN(0, arrSize[playerY] - 1);
 
                     futStep = map[playerY][playerX];
@@ -210,7 +207,6 @@ int main()
         case 'd':
         case 'D': {
             if (playerX < arrSize[playerY] - 1) {
-                cout << "right\n";
 
                 futStep = map[playerY][playerX + 1];
                 if (futStep >= '0' && futStep <= '9') {
@@ -223,11 +219,11 @@ int main()
 
                 if (futStep >= '0' && futStep <= '9' || futStep == '#' || futStep == ' ') {
                     map[playerY][playerX + 1] = 'Z';
-                    map[playerY][playerX] = '#';
+                    map[playerY][playerX] = ' ';
                     playerX++;
                 }
                 else if (futStep == '*') {
-                    map[playerY][playerX] = '#';
+                    map[playerY][playerX] = ' ';
                     playerY = randN(0, size - 1), playerX = randN(0, arrSize[playerY] - 1);
 
                     futStep = map[playerY][playerX];
@@ -241,16 +237,23 @@ int main()
             }
         }break;
 
-        default:  //nie obowązkowo
+        default:
             cout << "error\n";
         }
 
         steps--;
+        if(steps<=0){
+                cout << "\n\n===========Game over=============\n";
+            break;
+        }
+
         showMap(map, size, arrSize);
     }
+    short stepsFinish = steps;
 
-    cout << "============ Stat: \n" << "steps: " << steps << endl << "key press cout: " << keyPressCount << endl;
-    cout << "Score: " << steps * 1313.47674657 / keyPressCount << endl; //jeszcze nie skończyłam
+    cout << "======== Congratulations ========\n" <<  "============ Stat: ==============\n" << "Steps at the start: " << stepsStart << endl << "Steps at the finish: " << stepsFinish << endl;
+    cout << "Score: " << stepsFinish*100/stepsStart << "%" << endl;
 
     return 0;
 }
+
